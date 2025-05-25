77b46a9ee96afeae519022d4ef06ed4a7b233d58
tommyrios <torios@frba.utn.edu.ar>
2025-05-25 18:22:10 -0300

    Mejoro cohesion en Movimiento.java

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -3,12 +3,12 @@ package dds.monedero.model;
 import java.time.LocalDate;
 
 public class Movimiento {
-  private LocalDate fecha;
+  private final LocalDate fecha;
   // Nota: En ningún lenguaje de programación usen jamás doubles (es decir, números con punto flotante) para modelar dinero en el mundo real.
   // En su lugar siempre usen numeros de precision arbitraria o punto fijo, como BigDecimal en Java y similares
   // De todas formas, NO es necesario modificar ésto como parte de este ejercicio. 
-  private double monto;
-  private boolean esDeposito;
+  private final double monto;
+  private final boolean esDeposito;
 
   public Movimiento(LocalDate fecha, double monto, boolean esDeposito) {
     this.fecha = fecha;
@@ -24,24 +24,20 @@ public class Movimiento {
     return fecha;
   }
 
-  public boolean fueDepositado(LocalDate fecha) {
-    return isDeposito() && esDeLaFecha(fecha);
-  }
-
-  public boolean fueExtraido(LocalDate fecha) {
-    return isExtraccion() && esDeLaFecha(fecha);
-  }
-
-  public boolean esDeLaFecha(LocalDate fecha) {
-    return this.fecha.equals(fecha);
-  }
-
   public boolean isDeposito() {
     return esDeposito;
   }
 
-  public boolean isExtraccion() {
-    return !esDeposito;
+  public boolean esDelTipoYFecha(boolean tipoDeposito, LocalDate fecha) {
+    return this.esDeposito == tipoDeposito && this.fecha.equals(fecha);
+  }
+
+  public boolean fueDepositado(LocalDate fecha) {
+    return esDelTipoYFecha(true, fecha);
+  }
+
+  public boolean fueExtraido(LocalDate fecha) {
+    return esDelTipoYFecha(false, fecha);
   }
 
 }
